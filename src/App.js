8cca379beb642fe8eac2279d99ef95c1956e0b26
Taimoor M <taimoor.m89@gmail.com>
2021-01-06 21:31:15 -0500

    refactored code to use functional components and hooks

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import PageContent from "./PageContent";
 import { ThemeProvider, ThemeContext } from "./contexts/ThemeContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <LanguageProvider>
-          <PageContent>
-            <Navbar />
-            <Form />
-          </PageContent>
-        </LanguageProvider>
-      </ThemeProvider>
-    );
-  }
+function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
